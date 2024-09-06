552b1204e0a32200daf04f40d1f54e499a6e58c8
Алина Демчук <alina2805@bk.ru>
2024-09-06 16:30:24 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,19 @@
+calls = 0
+def count_calls():
+    global calls
+    calls = calls + 1
+def string_info(string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+def is_contains(string, list_to_search):
+    for i in list_to_search:
+        if string.lower() == i.lower():
+            return True
+    return False
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(calls)
